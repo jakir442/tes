acd8a1dee7176297da9bb422f8b44ea504b7d21c
jakir442 <jakigeming2021@gmail.com>
2023-12-10 17:23:51 +0700

    first

---
+++ src/app/components/layout/HomeMenu.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import MenuItem from '../menu/MenuItem';
+import SectionHeaders from './SectionHeaders';
+
+export default function HomeMenu() {
+  return (
+    <section className="">
+      <div className="absolute left-0 right-0 w-full justify-start">
+        <div className="h-48 w-48 absolute -top-[70px] left-0 text-left -z-10">
+          <Image src={'/sallad1.png'} alt={'sallad'} width={109} height={189} />
+        </div>
+        <div className="h-48 absolute -top-[100px] right-0 -z-10">
+          <Image src={'/sallad2.png'} alt={'sallad'} width={107} height={195} />
+        </div>
+      </div>
+      <div className="text-center mb-4">
+        <SectionHeaders subHeader={'Check out'} mainHeader={'Menu'} />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
+    </section>
+  );
+}
